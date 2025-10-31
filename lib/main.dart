e3d3cc767ada82f68304706bc0a5d91f17db5276
afnan923 <susumurni013@gmail.com>
2025-10-31 11:30:58 +0700

    create image box

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_splash/screen/splash_screen_1.dart';
 void main() {
   runApp(MyApp());
 }
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.amberAccent),
       ),
       debugShowCheckedModeBanner: false,
-      home: SplashScreen1(),
+      home: MySplashScreen(),
     );
       
   }
--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+
+class MySplashScreen extends StatelessWidget {
+  const MySplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 50),
+          Container(
+          width: 250,
+          height: 250,
+          decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.amber),
+          ),
+        ],
+      ),
+    );
   }
 }
