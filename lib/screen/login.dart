bfa7987f44d10e9aedf6d078ed4e6b63add841f0
afnan923 <susumurni013@gmail.com>
2025-10-31 14:10:36 +0700

    add text and icon

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -6,15 +6,30 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            appBar: AppBar(
-              title: const Text("Dashboard"),
-              actions: const [],
-            ),
-            body: SingleChildScrollView(
-              child: Container(
-                padding: const EdgeInsets.all(20.0),
-                child: const Column(
-                  children: [],
+            body: Center(
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsets.all(20.0),
+                  child: const Column(
+                    children: [
+                      SizedBox(height: 5,),
+                      Icon(Icons.qr_code_scanner_sharp, size: 60, color: Colors.blueGrey),
+                      SizedBox(height: 10,),
+                      Text("Selamat Datang",
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          color: Colors.blue
+                        ),
+                      ),
+                      SizedBox(height: 5,),
+                      Text("Silahkan Login untuk melanjutkan",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.green
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
